cd3975961e8a77aee215e3c3a40d72d0b8eba0d4
SalamatinVV <salamatin.vlad032@yandex.ru>
2024-04-11 15:28:20 +0300

    modified:   adder_tree_csa/src/_remainWire.sv

--- adder_tree_csa/src/_remainWire.sv
+++ adder_tree_csa/src/_remainWire.sv
@@ -1,5 +1,5 @@
-function automatic logic [31 : 0] _remainWire (input logic [31 : 0] x)
-
+function automatic logic [31 : 0] _remainWire (input logic [31 : 0] x) // реализация лишних входов, которые выходят из сумматоров
+                                                                       // на вход мы должны подавать колличество входов(для 1 цикла это колл-во sum[stage = 0])
     if (x % 3 == 0) begin
         _remainWire = 0                                                 ;
     end else begin
@@ -11,4 +11,28 @@ function automatic logic [31 : 0] _remainWire (input logic [31 : 0] x)
     end
     return _remainWire;
 endfunction
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// реализация если у нас 0 лишних проводов, 1 лишний, 2 лишних
 
+localparam [31 : 0] CSA_NUMBER = CSA_N(stage - 1)               ; 
+                for (genvar i = 0; i < CSA_NUMBER; i++) begin
+                    CSA_ff #(STAGE_W - 1) stagenum
+                    (
+                        .clk        (clk),
+                        .i_f        (sum[stage - 1][3 * i    ]                   )          ,
+                        .i_s        (sum[stage - 1][3 * i + 1]                   )          ,
+                        .i_t        (sum[stage - 1][3 * i + 2]                   )          ,
+                        .o_stage_s  (sum[stage    ][2*i      ][(STAGE_W - 1) : 0])          ,
+                        .o_stage_c  (sum[stage    ][2*i + 1  ][(STAGE_W - 1) : 0])      
+                    )       ;  
+                     if (_remainWire == 1) begin
+                        localparam [31 : 0] CSA_NUMBER_INSERT = CSA_N(stage)    ;
+                        always_comb begin
+                            sum[stage + 1][][STAGE_W : 0] = sum[stage][2*i + 1][{'0, (STAGE_W - 1 : 0)}]
+                        end
+                     end else if(_remainWire == 2) begin
+                        
+                     end else begin
+                        continue;
+                     end
+                end
