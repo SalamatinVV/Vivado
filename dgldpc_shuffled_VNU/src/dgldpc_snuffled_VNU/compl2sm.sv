9ac64ad3aabd42ac60a651d484998b28e05b350c
Salamatin.VV <salamatin.vlad032@yandex.ru>
2024-06-10 19:03:46 +0300

    modified:   dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/compl2sm.sv

--- dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/compl2sm.sv
+++ dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/compl2sm.sv
@@ -1,4 +1,4 @@
-module compl2sm                                                 // Из Дополнительного кода в Прямой код
+module compl2sm                                                 // �?з Дополнительного кода в Прямой код
     ( 
         input  logic [10 : 0] i_data                        ,
         output logic [10 : 0] o_data                                     
@@ -8,11 +8,11 @@ module compl2sm                                                 // Из Допо
 
     always_comb
     begin
-        if i_data[10] == 1 begin
+        if (i_data[10] == 1) begin
             sign_temp = '1                                  ;
         end else           begin
             sign_temp = '0                                  ;
         end
-        o_data = {i_data[10], i_data[9 : 0] ^ sign_temp}    ;
+        o_data = i_data[10] + i_data[9 : 0] ^ sign_temp     ;
     end
 endmodule
