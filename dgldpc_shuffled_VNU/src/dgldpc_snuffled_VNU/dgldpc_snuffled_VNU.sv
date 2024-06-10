0664f5874208b029d7804b82895c657fa71f60ab
Salamatin.VV <salamatin.vlad032@yandex.ru>
2024-06-10 23:35:29 +0300

    3	modified:   dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/dgldpc_snuffled_VNU.sv

--- dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/dgldpc_snuffled_VNU.sv
+++ dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/dgldpc_snuffled_VNU.sv
@@ -5,5 +5,24 @@ module dgldpc_shuffled_VNU
         input  logic [0 : 3][5 : 0] i_data                      ,
         output logic [0 : 4][9 : 0] o_data      
     );    
-    
+
+        logic [0 : 3][5 : 0] varCompl;
+        logic [0 : 1][6 : 0] sumFirst;
+        logic []
+
+    generate
+        for(genvar i = 0; i < 4; i++ ) begin
+            sm2compl sm2compl
+            (
+                .i_data (i_data[i]  )   ,
+                .o_data (varCompl[i])
+            );
+        end
+        always_comb
+        begin
+            sumFirst[0] = varCompl[0] + varCompl[1];
+            sumFirst[1] = varCompl[2] + varCompl[3];
+            sumSecond   = sumFirst[0] + sumFirst[1];
+        end
+    endgenerate
 endmodule
