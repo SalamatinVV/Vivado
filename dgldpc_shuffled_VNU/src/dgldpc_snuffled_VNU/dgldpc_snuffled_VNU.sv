15f1ef23df284998a4938d148a886a4bea133a0c
Salamatin.VV <salamatin.vlad032@yandex.ru>
2024-06-10 18:13:18 +0300

    modified:   dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/dgldpc_snuffled_VNU.sv 	modified:   dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/sm2compl.sv

---
+++ dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/dgldpc_snuffled_VNU.sv
@@ -0,0 +1,9 @@
+module dgldpc_shuffled_VNU
+    (
+        input  logic        [8 : 0] i_LOVNU                     ,
+        input  logic                clk                         , 
+        input  logic [0 : 3][5 : 0] i_data                      ,
+        output logic [0 : 4][9 : 0] o_data      
+    );    
+    
+endmodule
---
+++ dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/sm2compl.sv
@@ -0,0 +1,19 @@
+module sm2compl
+    ( 
+        input  logic [5 : 0] i_data                         ,
+        output logic [5 : 0] o_data                         ,
+        output logic         o_sign             
+    )                                                       ;
+
+    logic [4 : 0] compl_data                                ;
+    logic         sign_temp                                 ;
+
+    always_comb             
+    begin               
+        compl_data = | i_data[4 : 0]                        ;
+        o_data     = {i_data[5], i_data[4 : 0] ^ i_data[5]} ;
+        o_sign     = compl_data & i_data[5]                 ;                         
+    end
+
+
+endmodule
