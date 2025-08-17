d22c45c877a5aade951ed88035be2350746dd6f3
Саламатин Владислав <90028475+SalamatinVV@users.noreply.github.com>
2025-08-17 15:17:02 +0300

    Fix signed-magnitude conversions

--- dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/compl2sm.sv
+++ dgldpc_shuffled_VNU/src/dgldpc_snuffled_VNU/compl2sm.sv
@@ -1,19 +1,19 @@
-module compl2sm                                                 // �?з Дополнительного кода в Прямой код
-    ( 
+module compl2sm                                                 // Из Дополнительного кода в Прямой код
+    (
         input  logic [8 : 0] i_data                        ,
-        output logic [8 : 0] o_data                                     
+        output logic [8 : 0] o_data
     )                                                       ;
-    
-    logic [7 : 0] sign_temp                                 ;
-    logic [7 : 0] data                                      ;
-    always_comb
-    begin
-        if (i_data[8] == 1) begin
-            sign_temp = '1                                  ;
-        end else           begin
-            sign_temp = '0                                  ;
+
+    logic [7 : 0] magnitude                                 ;
+
+    always_comb begin
+        if (i_data[8]) begin
+            magnitude = (~i_data[7 : 0]) + 1                ;
+        end else begin
+            magnitude = i_data[7 : 0]                       ;
         end
-        data = (i_data[7 : 0] ^ sign_temp)                  ;
-        o_data = {sign_temp, i_data[8] + data}              ;
+        o_data = {i_data[8], magnitude}                     ;
     end
+
 endmodule
+
